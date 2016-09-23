38856c81e9752fc831bb39e5c36c86ee1d9aed8a
Schuyler Duveen <sky@skyb.us>
2016-09-22 23:02:41 -0400

    stalker stub

---
+++ github_stalker/stalker.js
@@ -0,0 +1,19 @@
+function getUser (username) {
+  $.ajax({
+    url: 'https://api.github.com/user/' + username '/events/',
+    dataType: 'json'
+    success: function(data) {
+      console.log('success', username, data);
+    },
+    error: function(evt) {
+    }
+  });
+}
+
+function getUsersByQuery(users) {
+  var userList = users.split(',');
+  $(userList).each(getUser);
+}
+
+getUsersByQuery(location.search.substr(1));
+
